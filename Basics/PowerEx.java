a9861e1981eb759a147d8252b1eaada22bae55ac
Nipoon <nipoon.donta7@gmail.com>
2020-10-01 12:07:19 +0530

    01/10

--- Basics/PowerEx.java
+++ Basics/PowerEx.java
@@ -4,6 +4,7 @@ public class PowerEx {
 public static void main(String[] args) {
     
         int power = (int)Math.pow(2,3);
+        System.out.println(Math.pow(2,3));
         System.out.println(power);
 }    
 }
