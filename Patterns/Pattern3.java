1caa9d2b2178d6b524e449ea545307798fa6ac63
Nipoon <nipoon.donta7@gmail.com>
2020-10-02 13:21:39 +0530

    Patterns

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Pattern3  {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        for( int i = 0 ; i < n ; i++ ){
+            int j;
+            for(j = 0 ; j < n; j++){
+                if(j < n - i -1)
+                {
+                    System.out.print("\t");
+                  
+                }
+                else{
+                    System.out.print("*\t");
+                }
+              
+            }
+            System.out.println();
+        }
+
+    }
+}
