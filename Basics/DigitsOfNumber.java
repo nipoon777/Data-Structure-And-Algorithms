bcd51ed7a488aec61285e8345b4f80de749dc297
nipoon777 <nipoon.donta7@gmail.com>
2020-10-02 13:54:25 +0530

    Laptop Changes

--- Basics/DigitsOfNumber.java
+++ Basics/DigitsOfNumber.java
@@ -1,3 +1,5 @@
+package Basics;
+
 import java.util.*;
 
 public class DigitsOfNumber {
--- Basics/Variables.java
+++ Basics/Variables.java
@@ -1,3 +1,4 @@
+package Basics;
 public class Variables {
     public static void main(String[] args) {
         int a = 10;
