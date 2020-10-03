7c6caae473303eeea50fbed5e5178e379f68e879
Nipoon <nipoon.donta7@gmail.com>
2020-10-03 18:13:55 +0530

    Pattern7

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+import java.util.*;
+
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        for(int i = 0 ; i < n ; i++){
+            int j;
+            for(j = 0 ; j < i ; j++){
+                System.out.print("\t");
+            }
+            System.out.println("*\t");
+        }
+        
+    }    
+}
