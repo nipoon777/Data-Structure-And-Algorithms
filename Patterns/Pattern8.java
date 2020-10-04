cf0daa3e4dce3c873296bdcf4cb7d5714c42d5e4
Nipoon <nipoon.donta7@gmail.com>
2020-10-04 17:06:38 +0530

    Pattern8

---
+++ Patterns/Pattern8.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+import java.util.*;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = n ; j > i ; j--)
+            {
+                System.out.print("\t");
+            }
+            System.out.println("*");
+        }
+    }    
+}
