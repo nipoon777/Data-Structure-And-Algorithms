e75b46233d52ec68a059f77058976961dfef8d25
Nipoon <nipoon.donta7@gmail.com>
2020-10-07 22:27:13 +0530

    Arrays

---
+++ FunctionsAndArrays/SubArray.java
@@ -0,0 +1,32 @@
+package FunctionsAndArrays;
+
+import java.io.*;
+import java.util.*;
+
+public class SubArray{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    
+    int[] a = new int[n];
+    
+    for(int i = 0 ; i < n ; i++){
+        a[i] = sc.nextInt();
+    }
+    
+    for(int i = 0 ; i < a.length ; i++){
+        for(int j = i ; j < a.length ; j++)
+        {
+            for(int k = i ; k <= j ; k++){
+                System.out.print(a[k] + "\t");
+            }
+            System.out.println();
+        }
+        
+    }
+ }
+
+}
---
+++ FunctionsAndArrays/SubSets.java
@@ -0,0 +1,40 @@
+package FunctionsAndArrays;
+
+import java.io.*;
+import java.util.*;
+
+public class SubSets{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    
+    int n = scn.nextInt();
+    int[] a = new int[n];
+    
+    for(int i = 0 ; i < n ; i++){
+        a[i] = scn.nextInt();    
+    }
+    
+    int length = (int)Math.pow(2, a.length);
+    
+    for(int i = 0 ;i < length ; i++){
+        String sb = "";
+        int temp =i;
+        
+        for(int j = a.length - 1 ; j >= 0 ; j--){
+            int rem = temp % 2;
+            temp = temp/2; 
+            
+            if ( rem == 0){
+                sb = "-\t" + sb ;
+            }
+            else{
+                sb =  a[j] +"\t" + sb;
+            }
+        }
+        System.out.println(sb);
+    }
+ }
+
+}
