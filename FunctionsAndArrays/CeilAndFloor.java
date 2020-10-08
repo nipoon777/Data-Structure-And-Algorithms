8fd34fef39a9d731281026397200a802f648f8e4
Nipoon <nipoon.donta7@gmail.com>
2020-10-08 13:23:05 +0530

    ArraysComplete

---
+++ FunctionsAndArrays/CeilAndFloor.java
@@ -0,0 +1,48 @@
+package FunctionsAndArrays;
+
+import java.io.*;
+import java.util.*;
+
+public class CeilAndFloor{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    
+    int n = scn.nextInt();
+    
+    int[] a = new int[n];
+    
+    for(int i  = 0 ;  i < n ; i++ ){
+        a[i] = scn.nextInt();
+    }
+    
+    int d = scn.nextInt();
+    
+    int lo = 0;
+    int hi = a.length - 1;
+    int ceil = 0;
+    int floor = 0;
+    
+    
+    while(lo <= hi){
+        
+        int mid = ( lo + hi ) / 2;
+        
+        if(a[mid] < d){
+            lo = mid + 1;
+            floor = a[mid];        
+        }else if (a[mid] > d){
+            hi = mid - 1;
+            ceil = a[mid];
+        }else{
+            floor = a[mid];
+            ceil = a[mid];
+        }
+    }
+    
+    System.out.println(ceil);
+    System.out.println(floor);
+    
+ }
+
+}
---
+++ FunctionsAndArrays/FirstAndLast.java
@@ -0,0 +1,66 @@
+package FunctionsAndArrays;
+
+import java.io.*;
+import java.util.*;
+
+public class FirstAndLast{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    
+    int n = scn.nextInt();
+    
+    int [] a = new int[n];
+    
+    for(int i = 0 ; i < n ; i++){
+        a[i] = scn.nextInt();    
+    }
+    
+    int d = scn.nextInt();
+    
+    int lo = 0;
+    int hi = a.length - 1;
+    
+    int fi = -1;
+    
+    while( lo <= hi){
+        int mid = (lo + hi) / 2;
+        
+        if(d > a[mid]){
+            lo = mid + 1;
+            
+        }else if (d < a[mid]){
+            hi = mid - 1;
+            
+        }else{
+            fi = mid;
+            hi = mid - 1;
+        }
+    }
+    System.out.println(fi);
+    
+    int li = -1;
+    lo = 0 ;
+    hi = a.length - 1;
+    
+    while( lo <= hi){
+        int mid = (lo + hi) / 2;
+        
+        if(d > a[mid]){
+            lo = mid + 1;
+            
+        }else if (d < a[mid]){
+            hi = mid - 1;
+            
+        }else{
+            li = mid;
+            lo = mid + 1;
+        }
+    }
+    System.out.println(li);
+    
+    
+    
+ }
+
+}
