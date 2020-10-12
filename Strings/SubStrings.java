6aa226432453a028ada0d7e7651252ad192adcee
Nipoon <nipoon.donta7@gmail.com>
2020-10-12 14:42:53 +0530

    Strings Start

---
+++ Strings/SubStrings.java
@@ -0,0 +1,18 @@
+package Strings;
+import java.util.*;
+
+
+public class SubStrings {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        String s = scn.nextLine();
+
+        for(int i = 0 ;i < s.length() ; i++){
+            for(int j = i + 1 ; j <= s.length();j++){
+                System.out.println(s.substring(i,j));
+            }
+        }
+        scn.close();
+    }    
+}
