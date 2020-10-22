c0e2a78aa18c95ca69722f6943b5a77ff2ce5ac5
nipoon777 <nipoon.donta7@gmail.com>
2020-10-22 14:08:50 +0530

    PrintKeyboardPattern

---
+++ RecursionWayUp/PrintKeyboardP.java
@@ -0,0 +1,38 @@
+package RecursionWayUp;
+
+import java.util.*;
+
+
+public class PrintKeyboardP {
+    public static void main(String[] args){
+        
+        Scanner scn = new Scanner(System.in);
+        
+        String ques = scn.next();
+        printKPC(ques, "");
+        
+        scn.close();
+
+    }
+    
+    static String[] codes = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    public static void printKPC(String ques, String ans) {
+        
+        if(ques.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        
+        char code = ques.charAt(0);
+        String roq = ques.substring(1);
+        
+        String codeString = codes[code - '0'];
+        
+        for(int i = 0 ; i < codeString.length() ; i++){
+            printKPC(roq, ans + codeString.charAt(i)); 
+        }
+        
+    }
+    
+}
--- RecursionWayUp/SubSequence.java
+++ RecursionWayUp/SubSequence.java
@@ -3,7 +3,7 @@ package RecursionWayUp;
 import java.util.*;
 
 public class SubSequence {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         String ques = scn.next();
         
