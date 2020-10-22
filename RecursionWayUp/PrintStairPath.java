c02472c834b75b48c6bec2ae3587265364e55263
nipoon777 <nipoon.donta7@gmail.com>
2020-10-22 14:21:59 +0530

    PrintStairPath

---
+++ RecursionWayUp/PrintStairPath.java
@@ -0,0 +1,31 @@
+package RecursionWayUp;
+import java.util.*;
+
+
+public class PrintStairPath {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        
+        printStairPaths(n,"");
+        
+        scn.close();
+
+    }
+
+    public static void printStairPaths(int n, String path) {
+        if(n == 0){
+            System.out.println(path);
+            return;
+        }else if(n < 0){
+            return;
+        }
+        
+        
+        for(int i = 1 ; i <= 3 ; i++){
+            printStairPaths(n - i , path + i);
+        }
+    }
+    
+}
