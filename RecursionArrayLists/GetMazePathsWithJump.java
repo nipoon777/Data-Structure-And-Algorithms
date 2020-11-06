f961369713496a32400dc0ba77fa262745abfcfa
nipoon777 <nipoon.donta7@gmail.com>
2020-11-06 17:00:33 +0530

    Streak

--- RecursionArrayLists/GetMazePathsWithJump.java
+++ RecursionArrayLists/GetMazePathsWithJump.java
@@ -53,7 +53,6 @@ public class GetMazePathsWithJump {
         }
         
         return paths;
-        
     }
     
 }
--- RecursionBackTracking/KnightsTour.java
+++ RecursionBackTracking/KnightsTour.java
@@ -1,4 +1,4 @@
-package RecursionWayUp;
+package RecursionBackTracking;
 
 import java.util.*;
 
