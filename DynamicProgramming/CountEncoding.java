deb3125f63521bb22c512c70253c824361c47759
nipoon777 <nipoon.donta7@gmail.com>
2020-11-15 22:08:42 +0530

    CountEncoding

---
+++ DynamicProgramming/CountEncoding.java
@@ -0,0 +1,45 @@
+package DynamicProgramming;
+
+import java.util.*;
+
+public class CountEncoding {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        
+        String str = scn.next();
+        scn.close();
+        int[] dp = new int[str.length()];
+        dp[0] = 1;
+        
+        for(int i = 1 ; i < dp.length ; i++){
+            if(str.charAt(i) == '0' && str.charAt(i - 1) == '0'){
+                dp[i] = 0;
+            }
+            
+            else if(str.charAt(i - 1) == '0' && str.charAt(i) != '0'){
+                dp[i] = dp[i - 1];
+            }
+            else if(str.charAt(i - 1) != '0' && str.charAt(i) == '0'){
+                if(str.charAt(i - 1) == '1' || str.charAt(i - 1) == '2'){
+                    dp[i] = (i >= 2 ? dp[i - 2] : 1);
+                }
+                else{
+                    dp[i] = 0;
+                }
+                
+            }else{
+                if(Integer.parseInt(str.substring(i - 1, i + 1)) <= 26){
+                    dp[i] = dp[i - 1] + (i >= 2 ? dp[i - 2] : 1);
+                }else{
+                    dp[i] = dp[i - 1];
+                }
+                
+            }
+        }
+        
+        System.out.println(dp[dp.length - 1]);
+        
+
+    }
+}
