9de18954f602eb6fb56f41129ed0cfdf0b6ece84
nipoon777 <nipoon.donta7@gmail.com>
2020-12-09 17:07:41 +0530

    StacksAndQueues

---
+++ StacksAndQueue/DuplicateBrackets.java
@@ -0,0 +1,38 @@
+package StacksAndQueue;
+
+import java.util.*;
+
+public class DuplicateBrackets {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        String str = scn.nextLine();
+        
+        Stack<Character> st = new Stack<>();
+        scn.close();
+        for(int i = 0 ; i < str.length() ; i++){
+            
+            char ch = str.charAt(i);
+            
+            if(ch == ')'){
+                if(st.peek() == '('){
+                    System.out.println("true");
+                    return;
+                }else{
+                    while(st.peek() != '('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+                
+            }else{
+                st.push(ch);
+            }
+        }
+        
+        System.out.println("false");
+
+    }
+
+}
