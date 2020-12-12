0714fa9861e3793ffccf85e9803d070e417b4aa9
nipoon777 <nipoon.donta7@gmail.com>
2020-12-12 12:18:11 +0530

    InfixEvaluation

---
+++ StacksAndQueue/InfixEvaluation.java
@@ -0,0 +1,83 @@
+package StacksAndQueue;
+
+import java.util.*;
+
+public class InfixEvaluation{
+  
+
+public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
+
+        Stack<Integer> oprnds = new Stack<>();
+        Stack<Character> optors = new Stack<>();
+
+        scn.close();
+        for(int i = 0 ; i < exp.length() ; i++){
+            char ch = exp.charAt(i);
+
+            if(ch == '('){
+                optors.push(ch);
+            }else if(Character.isDigit(ch)){
+                oprnds.push(ch - '0');
+            }else if(ch == ')'){
+                while( optors.peek() != '('){
+                    int val2 = oprnds.pop();
+                    int val1 = oprnds.pop();
+                    char optor = optors.pop();
+
+                    int operation = operation(val1, val2, optor);
+
+                    oprnds.push(operation);
+                }
+                optors.pop();
+            }else if(ch == '+' || ch == '-' || ch == '*' || ch =='/'){
+                while(!optors.isEmpty() && optors.peek() != '('
+                        && precedence(optors.peek()) >= precedence(ch)){
+                        int val2 = oprnds.pop();
+                        int val1 = oprnds.pop();
+                        char optor = optors.pop();
+
+                        int operation = operation(val1, val2, optor);
+
+                        oprnds.push(operation);
+                }
+                optors.push(ch);
+            }
+            
+        }
+         while(!optors.isEmpty()){
+                int val2 = oprnds.pop();
+                int val1 = oprnds.pop();
+                char optor = optors.pop();
+
+                int operation = operation(val1, val2, optor);
+
+                oprnds.push(operation);
+            }
+        System.out.println(oprnds.peek());
+    }
+    public static int operation(int val1, int val2, char optor){
+        if(optor == '+'){
+            return val1 + val2;
+        }else if(optor == '-'){
+            return val1 - val2;
+        }else if(optor == '*'){
+            return val1 * val2;
+        }else{
+            return val1 / val2;
+        }
+    }
+
+    public static int precedence(char optor){
+        if(optor == '+'){
+            return 1;
+        }else if(optor == '-'){
+            return 1;
+        }else if(optor == '*'){
+            return 2;
+        }else{
+            return 2;
+        }
+    }
+}
