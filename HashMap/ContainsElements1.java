f0241b368a854de5fa7cd84ace5031ec7585e8c3
nipoon777 <nipoon.donta7@gmail.com>
2021-01-21 16:37:45 +0530

    ContainsElements

---
+++ HashMap/ContainsElements1.java
@@ -0,0 +1,50 @@
+package HashMap;
+
+import java.util.*;
+
+public class ContainsElements1 {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        
+        int[] arr1 = new int[n1];
+        
+        for(int i = 0 ; i < arr1.length ; i++){
+            arr1[i] = scn.nextInt();
+        }
+        
+        int n2 = scn.nextInt();
+        
+        int[] arr2 = new int[n2];
+        
+        for(int i = 0 ; i < arr2.length ; i++){
+            arr2[i] = scn.nextInt();
+        }
+        
+        getCommonElements(arr1,arr2);
+    }
+    
+    public static void getCommonElements(int[] arr1, int[] arr2){
+        HashMap <Integer, Integer> hm = new HashMap<>();
+        
+        for(int val : arr1){
+            if(hm.containsKey(val)){
+                int nf = hm.get(val) + 1;
+                hm.put(val,nf);
+            }else{
+                hm.put(val, 1);
+            }
+        }
+        
+        for(int val : arr2){
+            if(hm.containsKey(val)){
+                System.out.println(val);
+                hm.remove(val);
+            }
+        }
+    }
+    
+
+}
