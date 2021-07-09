cad4bb888c6d82828caa14daea6e00243894eb6c
nipoon777 <nipoon.donta7@gmail.com>
2021-07-09 21:52:27 +0530

    Merge k Sorted Array

---
+++ Level2/LinkedList/MergeKSortedPriority.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue <ListNode> pq = new PriorityQueue<>((a, b)-> {
+          return a.val - b.val;  
+        });
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        for(ListNode l : lists) if( l != null ) pq.add(l);
+        
+        while( pq.size() > 0 ){
+            ListNode node = pq.remove();
+            prev.next = node;
+            prev = prev.next;
+            node = node.next;
+            
+            if( node != null ) pq.add(node);
+        }
+        
+        return dummy.next;
+        
+    }
+}
